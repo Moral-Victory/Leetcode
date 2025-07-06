088e3cfa9e67f593bc0a15419b04a7980eea2bfa
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2025-07-06 20:08:30 +0530

    Time: 476 ms (20.79%) | Memory: 8.7 MB (87.85%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool satisfies(char num, vector<vector<char>>& board, int row, int col) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+            if (board[row][i] == num) {
+                return false;
+            }
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (satisfies(num, board, i, j)) {
+                            board[i][j] = num;
+
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false; // if nothing found after trying out
+                                  // everything
+                } // for that position
+            }
+        }
+        return true; // if, no position is empty so solved by default
+    }
+
+    void solveSudoku(vector<vector<char>>& board) { solve(board); }
+};
