d81fd61b7439017a3193d40957f4e6727d42e7a5
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2025-12-27 11:22:21 +0530

    Time: 3 ms (59.77%), Space: 14 MB (46.88%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string expression) {
+       vector<int> ans; 
+
+        for(int i=0; i<expression.size(); i++){
+            if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*'){
+                vector<int> left = diffWaysToCompute(expression.substr(0, i));
+                vector<int> right = diffWaysToCompute(expression.substr(i+1));
+
+                for(int x: left){
+                    for(int y: right){
+                        if(expression[i]=='+'){
+                            ans.push_back(x+y);
+                        }
+                        else if(expression[i]=='-'){
+                            ans.push_back(x-y);
+                        }
+                        else{
+                            ans.push_back(x*y);
+                        }
+                    }
+                }
+            }
+        }
+        if(ans.empty()){
+            ans.push_back(stoi(expression));
+        } 
+        return ans;
+    }
+};
