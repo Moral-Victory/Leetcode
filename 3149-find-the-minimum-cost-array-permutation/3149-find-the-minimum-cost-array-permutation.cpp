071670b646b6d77de4c0f0d3e4953ede23ecef99
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2026-01-05 14:12:20 +0530

    Time: 2874 ms (5.56%), Space: 30.2 MB (71.3%) - LeetHub

---
+++ 3149-find-the-minimum-cost-array-permutation/3149-find-the-minimum-cost-array-permutation.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int n;
+    int minScore = INT_MAX;
+    vector<int> resultPerm;
+
+    void solve(vector<int>& nums, vector<bool>& visited, vector<int>& perm,
+               int score) {
+        if(score>minScore){
+            return;
+        }
+
+        // Base case: full permutation formed
+        if (perm.size() == n) {
+            score += abs(perm.back() - nums[perm[0]]); // close the cycle
+
+            if (score < minScore) {
+                minScore = score;
+                resultPerm = perm;
+            }
+            return;
+        }
+        // Try all unused numbers
+        for (int num = 0; num < n; num++) {
+            if (!visited[num]) {
+                visited[num] = true;
+                perm.push_back(num);
+
+                int s = perm.size();
+                solve(nums, visited, perm,
+                      score + abs(perm[s - 2] - nums[perm[s - 1]]));
+                perm.pop_back();
+                visited[num] = false;
+            }
+        }
+    }
+
+    vector<int> findPermutation(vector<int>& nums) {
+        n = nums.size();
+        vector<bool> visited(n, false);
+        vector<int> perm = {0};
+        visited[0] = true;
+        solve(nums, visited, perm, 0);
+        return resultPerm;
+    }
+};
