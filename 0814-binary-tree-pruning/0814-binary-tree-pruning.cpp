7c60cd81990178a79eec3c0a35519eb52c5322fd
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2026-01-08 00:17:58 +0530

    Time: 0 ms (100%), Space: 12.4 MB (75.43%) - LeetHub

---
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int solve(TreeNode* root){
+        if(root==nullptr){
+            return 0;
+        }
+        int l = solve(root->left);
+        int r = solve(root->right);
+        int sum=l + r + root->val;
+        if(l==0){
+            root->left=nullptr;
+        }
+        if(r==0){
+            root->right=nullptr;
+        }
+        return sum;
+    }
+
+    TreeNode* pruneTree(TreeNode* root) {
+        if(solve(root)==0){
+            return nullptr;
+        }
+        return root;
+    }
+};
