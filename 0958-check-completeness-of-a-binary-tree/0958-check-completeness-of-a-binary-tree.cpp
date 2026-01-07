cfbcca4c490e59a7f1a0175065c664f257aa2c9c
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2026-01-07 18:33:14 +0530

    Time: 0 ms (100%), Space: 13.9 MB (33.01%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+        bool seen_null = false;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            if (node == nullptr) {
+                seen_null = true;
+            } else {
+                if (seen_null) {
+                    return false;
+                }
+                q.push(node->left);
+                q.push(node->right);
+            }     
+        }
+        return true;
+    }
+};
