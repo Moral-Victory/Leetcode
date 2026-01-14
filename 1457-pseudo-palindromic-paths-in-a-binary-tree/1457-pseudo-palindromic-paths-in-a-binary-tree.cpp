8b8123c8e8378aa4d2871c1823e17fad7a435e79
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2026-01-14 22:29:34 +0530

    Time: 2 ms (92.72%), Space: 181.2 MB (88.08%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -12,34 +12,50 @@
 class Solution {
 public:
     int ans=0;
-    void solve(TreeNode* root, vector<int>& count){
+
+    // void solve(TreeNode* root, vector<int>& count){
+    //     if(root==nullptr){
+    //         return; 
+    //     }
+    //     count[root->val]++;
+    //     if(root->left==nullptr && root->right==nullptr){
+    //         int oddfreq=0;
+    //         for(int i=0; i<count.size(); i++){
+    //             if(count[i]%2==1){
+    //                 oddfreq++;
+    //             }
+    //         }
+    //         if(oddfreq>1){
+    //             count[root->val]--;
+    //             return;
+    //         }
+    //         ans++;
+    //         count[root->val]--;
+    //         return;
+    //     }
+    //     solve(root->left, count);
+    //     solve(root->right, count);
+    //     count[root->val]--;
+    // }
+
+    void solve(TreeNode* root, int count){
         if(root==nullptr){
             return; 
         }
-        count[root->val]++;
+        count=(count^(1<<root->val));
         if(root->left==nullptr && root->right==nullptr){
-            int oddfreq=0;
-            for(int i=0; i<count.size(); i++){
-                if(count[i]%2==1){
-                    oddfreq++;
-                }
+            if((count&(count-1))==0){
+                 ans+=1;
             }
-            if(oddfreq>1){
-                count[root->val]--;
-                return;
-            }
-            ans++;
-            count[root->val]--;
-            return;
         }
         solve(root->left, count);
         solve(root->right, count);
-        count[root->val]--;
     }
 
     int pseudoPalindromicPaths (TreeNode* root) {
-        vector<int> count(10, 0);
-        solve(root, count);
+        // vector<int> count(10, 0);
+        int count;
+        solve(root, 0);
         return ans;
     }
 };
