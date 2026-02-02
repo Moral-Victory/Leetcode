5eaae39bd73ae49a5334cd48f4ed551738c6b9b3
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2026-02-02 21:37:48 +0530

    Time: 0 ms (100%), Space: 19.9 MB (23.41%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
+        // int ans=letters[0];
+        // for(int i=0; i<letters.size(); i++){
+        //     if(letters[i]>target){
+        //         ans=letters[i];
+        //         break;
+        //     }
+        // }
+        // return ans;
+        int n=letters.size();
+        int low=0;
+        int high=n-1;
         int ans=letters[0];
-        for(int i=0; i<letters.size(); i++){
-            if(letters[i]>target){
-                ans=letters[i];
-                break;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(letters[mid]<=target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+                ans=letters[mid];
             }
         }
         return ans;
