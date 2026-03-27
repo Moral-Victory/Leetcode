7c8e8b654e7ab8d0a2b65819315af9980fe2c42f
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2026-03-28 01:24:05 +0530

    Time: 0 ms (100%), Space: 16.3 MB (39.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -20,6 +20,27 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m-1, n-1, grid, dp);
+        // return solve(m-1, n-1, grid, dp);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    dp[i][j]=grid[0][0];
+                }
+                else{
+                    int left=INT_MAX;
+                    int up=INT_MAX;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] = grid[i][j]+min(left, up);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+
+
     }
 };
