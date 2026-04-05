ce439267c511f1232f91b252e4c119bb1c84ed40
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2026-04-06 02:00:31 +0530

    Time: 0 ms (100%), Space: 12.9 MB (57.5%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -14,19 +14,23 @@ public:
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
         // return solve(0, 0, triangle, n, m, dp);
+        vector<int> dp(n, 0);
         for(int i=0; i<n; i++){
-            dp[n-1][i]=triangle[n-1][i];
+            dp[i]=triangle[n-1][i];
         }
+
         for(int i=n-2; i>=0; i--){
+            vector<int> curr(n, 0);
             for(int j=0; j<=i; j++){
-                int down=triangle[i][j]+dp[i+1][j];
-                int down_diagonal=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down, down_diagonal);
+                int down=triangle[i][j]+dp[j];
+                int down_diagonal=triangle[i][j]+dp[j+1];
+                curr[j]=min(down, down_diagonal);
             }
+            dp=curr;
         }
-        return dp[0][0];
+        return dp[0];
         
     }
 };
