b2aaae282fc9e2fc57f42865ad5b9fa2e09376b7
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2026-06-02 02:04:49 +0530

    Time: 39 ms (34.2%), Space: 61.4 MB (9.78%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -19,7 +19,30 @@ public:
     int change(int amount, vector<int>& coins) {
         
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return solve(n-1, amount, coins, dp);
+        vector<vector<unsigned long long>> dp(n, vector<unsigned long long>(amount+1, 0));
+        
+        for(int j=0; j<amount+1; j++){
+            if(j%coins[0]==0){
+                dp[0][j]=1;
+            }
+            else{
+                dp[0][j]=0;
+            }
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j<amount+1; j++){
+                unsigned long long not_take=dp[i-1][j];
+                unsigned long long take=0;
+                if(coins[i]<=j){
+                    take=dp[i][j-coins[i]];
+                }
+                dp[i][j]=not_take+take;
+            }
+        }
+        return (int)dp[n-1][amount];
+
+
+        // return solve(n-1, amount, coins, dp);
     }
 };
