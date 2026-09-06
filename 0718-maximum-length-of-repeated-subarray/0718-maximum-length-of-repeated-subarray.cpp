c6ad3b511a20691beb555f737965a0d942261e12
Moral-Victory <159948395+Moral-Victory@users.noreply.github.com>
2026-09-06 13:09:13 +0530

    Time: 47 ms (89.68%), Space: 14.7 MB (91.65%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -3,24 +3,27 @@ public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int m=nums1.size();
         int n=nums2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        // vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        vector<int> curr(n+1, -1);
+        vector<int> prev(n+1, -1);
         for(int i=0; i<=n; i++){
-            dp[0][i]=0;
+            prev[i]=0;
         }
-        for(int i=0; i<=m; i++){
-            dp[i][0]=0;
-        }
+        curr[0]=0;
+
         int ans=0;
         for(int i=1; i<=m; i++){
             for(int j=1; j<=n; j++){
                 if(nums1[i-1]==nums2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                    ans=max(ans, dp[i][j]);
+                    curr[j]=1+prev[j-1];
+                    ans=max(ans, curr[j]);
                 }
                 else{
-                    dp[i][j]=0;
+                    curr[j]=0;
                 }
             }
+            prev=curr;
+            curr[0]=0;
         }
         return ans;
 
